90a7de9a7bb1fdf686f58e5e280f62f38a878e72
TLazovic-CentennialCollege <tlazovic@my.centennialcollege.ca>
2025-04-07 17:26:30 -0400

    folder structure created

---
+++ client/business-app/src/components/residentCommunity/BulletinBoard/IndividualDiscussion.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const BulletinBoard = () => {
+  return <h1>BulletinBoard</h1>;
+};
+
+export default BulletinBoard;
---
+++ client/business-app/src/components/residentCommunity/ResidentDashboard.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const ResidentDashboard = () => {
+  return <h1>Resident Dashboard</h1>;
+};
+
+export default ResidentDashboard;
