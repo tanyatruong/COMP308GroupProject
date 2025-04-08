23eb0ca8f5b59e2e13369fe79de2ddb3e546c21b
Landon Essex <61153703+landonessex@users.noreply.github.com>
2025-04-08 13:27:09 -0400

    update gateway.js

--- server/gateway.js
+++ server/gateway.js
@@ -18,6 +18,7 @@ const gateway = new ApolloGateway({
             { name: 'auth', url: 'http://localhost:4001/graphql' },
             { name: 'business', url: 'http://localhost:4002/graphql' },
             { name: 'ai', url: 'http://localhost:4003/graphql' },
+            { name: 'community', url: 'http://localhost:4004/graphql' },
         ],
     }),
     buildService({ name, url }) {
