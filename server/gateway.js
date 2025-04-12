e8b59e1aa9c64e725e4aa3a850d1cf3a2ac27d6d
TLazovic-CentennialCollege <tlazovic@my.centennialcollege.ca>
2025-04-12 16:34:55 -0400

    updated Gateway

--- server/gateway.js
+++ server/gateway.js
@@ -22,9 +22,9 @@ const gateway = new ApolloGateway({
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [
       { name: "community", url: "http://localhost:4004/graphql" },
-      //   { name: "auth", url: "http://localhost:4001/graphql" },
-      //   { name: "business", url: "http://localhost:4002/graphql" },
-      //   { name: "ai", url: "http://localhost:4003/graphql" },
+      { name: "auth", url: "http://localhost:4001/graphql" },
+      { name: "business", url: "http://localhost:4002/graphql" },
+      { name: "ai", url: "http://localhost:4003/graphql" },
     ],
   }),
   buildService({ name, url }) {
