87694ed7d8f22127f5c69c17057799bbc9e1ebc4
Landon Essex <61153703+landonessex@users.noreply.github.com>
2025-04-14 11:55:05 -0400

    update resolvers.js

--- server/community-service/graphql/resolvers.js
+++ server/community-service/graphql/resolvers.js
@@ -1,5 +1,5 @@
-const { Post } = require('../models/Post');
-const { Comment } = require('../models/Comment');
+const Post = require('../models/Post');
+const Comment = require('../models/Comment');
 
 const resolvers = {
   Post: {
